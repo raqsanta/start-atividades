754ef7f7b78aaf59b869982bcbeca1664cd04c77
ggkei <eahisla@gmail.com>
2022-06-05 00:42:27 -0300

    Envio de Atividade

---
+++ estruturas-de-repeticao/atividade-02/questao-01.js
@@ -0,0 +1,14 @@
+
+//https://academia.iphac.org.br/mod/assign/view.php?id=119072
+
+//1)      Faça uma função que exibe todos os números maiores que zero e menores que 10.
+
+function showNumbers(){
+
+    for(let i=1; i < 10; i++){
+        console.log(i)
+    }
+
+}
+
+showNumbers()
---
+++ estruturas-de-repeticao/atividade-02/questao-02.js
@@ -0,0 +1,15 @@
+
+//https://academia.iphac.org.br/mod/assign/view.php?id=119072
+
+//2)      Faça uma função que exibe os 10 primeiros números que são menores que 80.
+
+
+function showNumbers(max){
+
+    for(let i=(max-10); i < max; i++){
+        console.log(i)
+    }
+
+}
+
+showNumbers(80)
