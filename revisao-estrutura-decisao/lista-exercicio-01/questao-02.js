1525286d321842c1395660e554776754441edfe0
ggkei <eahisla@gmail.com>
2022-06-05 01:40:52 -0300

    Envio de Atividade

---
+++ revisao-estrutura-decisao/lista-exercicio-01/questao-02.js
@@ -0,0 +1,10 @@
+
+//https://academia.iphac.org.br/mod/assign/view.php?id=119104
+
+function hWorld(){
+
+    for(let i=0; i<=15; i++){
+        console.log('hello world')
+    }
+
+}
---
+++ revisao-estrutura-decisao/lista-exercicio-01/questao-03.js
@@ -0,0 +1,14 @@
+
+//https://academia.iphac.org.br/mod/assign/view.php?id=119104
+
+function showNumbers(max){
+
+    for(let i=(max+1); i <= (max+6); i++){
+
+        console.log(i)
+
+    }
+
+}
+
+showNumbers(88)
