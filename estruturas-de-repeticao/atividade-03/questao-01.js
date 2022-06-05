d9be39fee09d031b29836a26b1f5b4a6664e466f
ggkei <eahisla@gmail.com>
2022-06-05 00:49:34 -0300

    Envio de Atividade

---
+++ estruturas-de-repeticao/atividade-03/questao-01.js
@@ -0,0 +1,15 @@
+
+// https://academia.iphac.org.br/mod/assign/view.php?id=119075
+
+//1)      Faça uma função que escreve os números de 1 a 10 em ordem crescente.
+
+
+function showNumbers(){
+
+    for(let i=1; i <= 10; i++){
+        console.log(i)
+    }
+
+}
+
+showNumbers()
---
+++ estruturas-de-repeticao/atividade-03/questao-02.js
@@ -0,0 +1,15 @@
+
+// https://academia.iphac.org.br/mod/assign/view.php?id=119075
+
+//2)      Faça uma função que escreve os números de 10 a 1 em ordem decrescente
+
+
+function showNumbers(){
+
+    for(let i=10; i <= 1; i--){
+        console.log(i)
+    }
+
+}
+
+showNumbers()
